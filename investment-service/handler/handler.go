94e578a7f3e8e950bb08f4a3b015cc4e84d92d32
Oli Knight <olidknight@gmail.com>
2025-06-17 22:04:57 +0100

    feat(investmentService): CreateInvestment implementation

---
+++ investment-service/handler/handler.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/oliknight1/retail-isa-investment/investment-service/internal"
+	"github.com/oliknight1/retail-isa-investment/investment-service/service"
+)
+
+type InvestmentHandler struct {
+	Service service.InvestmentService
+}
+
+func New(service service.InvestmentService) *InvestmentHandler {
+	return &InvestmentHandler{service}
+}
+
+func (h *InvestmentHandler) CreateInvestment(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		CustomerId string  `json:"customerId"`
+		FundId     string  `json:"fundId"`
+		Amount     float64 `json:"amount"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, fmt.Sprintf("error reading JSON: \n%s", err), http.StatusBadRequest)
+		return
+	}
+
+	if req.CustomerId == "" {
+		http.Error(w, internal.ErrMissingCustomerId.Error(), http.StatusBadRequest)
+		return
+	}
+	if req.FundId == "" {
+		http.Error(w, internal.ErrMissingFundId.Error(), http.StatusBadRequest)
+		return
+	}
+	if req.Amount <= 0 {
+		http.Error(w, internal.ErrZeroTransactionAmount.Error(), http.StatusBadRequest)
+		return
+	}
+
+	transaction, err := h.Service.CreateInvestment(req.CustomerId, req.FundId, req.Amount)
+
+	if err != nil {
+		http.Error(w, "failed to create investment", http.StatusInternalServerError)
+		return
+	}
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(transaction); err != nil {
+		log.Printf("failed to encode JSON: %v", err)
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(buf.Bytes())
+}
